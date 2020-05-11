048327e5375db5f49247066185035826190dbbf5
rydawnshootingstar <rydawnshootingstar@gmail.com>
2020-05-11 13:06:31 -0400

    added dummy data

---
+++ itemData.js
@@ -0,0 +1,38 @@
+module.exports = [
+	{
+		name: "Coffee Cup",
+		cost: "2.50"
+	},
+	{
+		name: "Big Mug",
+		cost: "5.00"
+	},
+	{
+		name: "Aeropress",
+		cost: "25.00"
+	},
+	{
+		name: "Coffee Cup",
+		cost: "2.50"
+	},
+	{
+		name: "Big Mug",
+		cost: "5.00"
+	},
+	{
+		name: "Aeropress",
+		cost: "25.00"
+	},
+	{
+		name: "Coffee Cup",
+		cost: "2.50"
+	},
+	{
+		name: "Big Mug",
+		cost: "5.00"
+	},
+	{
+		name: "Aeropress",
+		cost: "25.00"
+	}
+];
